466a6bca7e8ae7904d5f291af5fbceffb021b9a6
Trabajo de c++ <septiembre135@outlook.com>
2019-12-15 18:06:08 +0100

    Initial commit

---
+++ BestFroggerEver/BestFroggerEver.cpp
@@ -0,0 +1,26 @@
+#include <iostream>  
+#include <Windows.h>  
+#include "FAriasSimpleGraphics.h"
+
+void SquarePixels();
+
+void SquarePixels()
+{
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 10;                   // Width of each character in the font
+	cfi.dwFontSize.Y = 10;                  // Height
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
+
+int main()
+{
+	SquarePixels();
+}
+
+/*		COSAS A RECORDAR
+-	Aprenderse SquarePixels
+*/
